be60d9ac0dafafc9613555ed625459f24263827d
Hilton Lam <hiltonlam195@gmail.com>
2021-01-05 11:39:42 -0800

    Quit implemented

--- main.py
+++ main.py
@@ -16,27 +16,54 @@ THE CAR IS SPECIAL
 THAT'S WHY THE GOVERNMENT WANTS IT.
 
 WE CAN'T LET THEM HAVE IT.
-ONE GOAL: SURVIAL... and THE CAR
-REACH THE END BEFORE THE MAN GETS IT."""
+ONE GOAL: SURVIVAL... and THE CAR
+REACH THE END BEFORE THE MAN GETS IT.
 
 
-def main():
-    # Display introduction
+
+-----
+"""
+
+CHOICES = """
+    ----
+    Q.QUIT
+    ----
+"""
+
+
+def intro():
     for char in textwrap.dedent(INTRODUCTION):
         time.sleep(0.05)
         sys.stdout.write(char)
         sys.stdout.flush()
+def main():
+    # intro()
 
     time.sleep(1)
 
+
+    # Variables
+    done = False
     # MAIN LOOP
+    while not done:
+        pass
         # TODO: Check if reached END GAME
 
-        # TODO: Present the user their choices
+        #  Present the user their choices
+        print(CHOICES)
+
+        user_choice = input("What do you want to do? "). lower().strip("!,,?")
+
+        if user_choice ==  "q":
+            done = True
+
 
         # TODO Change the environment based on
         #   user choice, and RNG
         # TODO: Random event generator
+
+    # Outro
+    print("Thanks for playing. Play again soon!")
 
 
 if __name__ == "__main__":
